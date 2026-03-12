4b9ff8b1946164e17977de099e49ab5926a0f233
dylcaton <dylanpcaton@gmail.com>
2026-03-12 13:54:24 -0500

    props

---
+++ src/BlogPost.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="blog-post">
+        <p>{{ message }}</p>
+
+        <h2>{{ id }} — {{ blogPostTitle }}</h2>
+        <h4>{{ blogPostContent }}</h4>
+        <button @click="changeBlogPostTitle">Change blog post title</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+let message = ref('This is the BlogPost component.');
+let props = defineProps(['id','blogPostTitle', 'blogPostContent'])
+console.log(props.blogPostTitle);
+// defineProps({
+//     id: Number,
+//     blogPostTitle: String,
+//     blogPostContent: String
+// })
+function blogPostTitle() {
+    props.blogPostTitile = 'A different title';
+}
+
+</script>
+
+<style scoped>
+.blog-post {
+    background-color: aqua;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+
+</style>
